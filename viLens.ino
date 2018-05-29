5c63a560c0de73ee351e485d7b2db2b39a248c51
harishandzic <38948444+harishandzic@users.noreply.github.com>
2018-05-29 12:55:13 +0200

    ViLens-3sensors

--- viLens.ino
+++ viLens.ino
@@ -1,31 +1,20 @@
 /* Postavka */
 #include<NewPing.h>
+#include<String.h>
 const int leftTriggerPin = 6;
 const int leftEchoPin = 7;
 const int frontTriggerPin = 8;
 const int frontEchoPin = 9;
-const int rightTriggerPin = 10;
-const int rightEchoPin = 11;
-
+const int rightTriggerPin = 11;
+const int rightEchoPin = 10;
 
 //varijable
- float maxFrontUdaljenost = 100.00;
-float frontDuration, frontDistanceCm, leftDuration, leftDistanceCm,rightDistanceCm,rightDuration,normalnaVisina,brojac=0;
-void NormalnaVisina()
-{
-  float suma=0;
-  for(int i=0;i<100;i++)
-  {  
-  NewPing sonar(frontTriggerPin, frontEchoPin, 200); 
-  if(sonar.ping_cm()!=0)
-     {
-      suma+=sonar.ping_cm();
-      brojac++;
-     }
-  }
-  normalnaVisina=suma/brojac;
-}
-
+float vrijednost = 150;
+float vrijednostLR=120;
+float maxGledanaUdaljenost = 200.00;
+float frontDuration, frontDistanceCm, leftDuration, leftDistanceCm,rightDistanceCm,rightDuration,normalnaVisina,brojac=0,razlika,suma1=0,suma2=0,h1,h2,r=4.2,l1,l2,l3,rw=20,s=20;
+String smjer;
+int frontUdaljenostMjera,leftUdaljenostMjera,rightUdaljenostMjera;
 void setup() {
   pinMode(frontTriggerPin,OUTPUT);
   pinMode(frontEchoPin,INPUT);
@@ -33,59 +22,58 @@ void setup() {
   pinMode(rightEchoPin,INPUT);
   pinMode(leftTriggerPin,OUTPUT);
   pinMode(leftEchoPin,INPUT);
-  NormalnaVisina();
   Serial.begin(9600);
 }
 
 void loop() {
- 
+  leftUdaljenost();
+  delay(50);
+  rightUdaljenost();
+  delay(50);
   frontUdaljenost();
-  float vrijednostVisina=frontDistanceCm-normalnaVisina;
-  if(vrijednostVisina>5)
+  delay(50);
+  h1=(leftDistanceCm/2)+21;
+  h2=(rightDistanceCm/2)+21;
+  l1=leftDistanceCm*0.866-5.63;
+  l2=(rightDistanceCm-leftDistanceCm)*0.866;
+  l3=(frontDistanceCm-rightDistanceCm)*0.866-5.63;
+  if((h1>rw+s) && (h2>rw+s))
   {
-    Serial.print(vrijednostVisina);
-    Serial.println("PREPREKA-PAZI");
+    smjer="P";
+    Serial.print(smjer);
+    Serial.print("|");
+    //mozete nastaviti naprijed
+   }
+   else if(leftDistanceCm>rightDistanceCm)
+   {
+    smjer="P1";
+    Serial.print(smjer);
+    Serial.print("|");
+    //skrenite lijevo da izbjegnete prepreku
+   }
+   else if(rightDistanceCm>leftDistanceCm)
+   {
+    smjer="P2";
+   Serial.print(smjer);
+   Serial.print("|");
+    //skrenite desno da izbjegnete prepreku
+   }
   }
-  else if(vrijednostVisina<-5)
-  {
-    Serial.print(vrijednostVisina);
-    Serial.println("IDETE DOLE");
-  }
-  else{
-    Serial.println("OK JE");
-  }
- 
-  
-  
-}
-void frontUdaljenost(){
-  do{
-    NewPing sonar(frontTriggerPin, frontEchoPin, 200); 
-    frontDistanceCm=sonar.ping_cm();
-  }while(frontDistanceCm==0);
- }
-
  void leftUdaljenost(){
-  digitalWrite(leftTriggerPin, LOW);  
-  delayMicroseconds(4);
-  digitalWrite(leftTriggerPin, HIGH);  
-  delayMicroseconds(10);
-  digitalWrite(leftTriggerPin, LOW);
-  leftDuration = pulseIn(leftEchoPin, HIGH);  
-  leftDistanceCm = leftDuration * 10 / 292 / 2;
-  /* Serial.print("Udaljenost: ");
-  Serial.print(leftDistanceCm);
-  Serial.println(" cm");*/
+   do{
+    NewPing sonar(leftTriggerPin, leftEchoPin, maxGledanaUdaljenost); 
+    leftDistanceCm=sonar.ping_cm();
+  }while(leftDistanceCm==0);
  }
   void rightUdaljenost(){
-  digitalWrite(rightTriggerPin, LOW);  
-  delayMicroseconds(4);
-  digitalWrite(rightTriggerPin, HIGH);  
-  delayMicroseconds(10);
-  digitalWrite(rightTriggerPin, LOW);
-  rightDuration = pulseIn(rightEchoPin, HIGH);  
-  rightDistanceCm = rightDuration * 10 / 292 / 2;
-  /*  Serial.print("Udaljenost: ");
-  Serial.print(rightDistanceCm);
-  Serial.println(" cm");*/
- }
+   do{
+    NewPing sonar(rightTriggerPin, rightEchoPin, maxGledanaUdaljenost); 
+    rightDistanceCm=sonar.ping_cm();
+  }while(rightDistanceCm==0);
+}
+void frontUdaljenost(){
+   do{
+    NewPing sonar(frontTriggerPin, frontEchoPin, maxGledanaUdaljenost); 
+    frontDistanceCm=sonar.ping_cm();
+  }while(frontDistanceCm==0);
+}
